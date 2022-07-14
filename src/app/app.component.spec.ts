4c8ab733bc4d5ab39268c3705c1527e07507342a
Utsav1417 <utsavspanchal2001@gmail.com>
2022-07-14 21:48:02 +0530

    Second Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crypto-checker-yt'`, () => {
+  it(`should have as title 'crypto-trend-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crypto-checker-yt');
+    expect(app.title).toEqual('crypto-trend-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crypto-checker-yt app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crypto-trend-tracker app is running!');
   });
 });
